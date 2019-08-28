fe6991f2351565c8056f4e3d6a3cbab27f7c4fd1
Time <244439359@qq.com>
2019-08-29 00:01:12 +0800

    add attr

--- crawler.js
+++ crawler.js
@@ -1,63 +1,123 @@
-const cheerio = require('cheerio')
-const axios = require('axios')
-const urlAll = []
+function crawler(num) {
+    const cheerio = require('cheerio')
+    const axios = require('axios')
+    const mongoose = require('mongoose')
+    const urlAll = []
+    const n = num;
 
-!(async function getURL() {
-    const request = await axios.get('https://www.douban.com/group/explore')
+    mongoose.connect('mongodb://localhost/doubanData')
 
-    const document = request.data
+    const db = mongoose.connection
 
-    const $ = cheerio.load(document)
+    db.on('error', function (err) {
+        console.log("数据库连接失败：" + err);
+    });
+    db.once('open', function () {
+        console.log("数据库连接成功");
+    });
+    db.once('disconnected', function () {
+        console.log("数据库连接断开");
+    });
 
-    const list = $('.channel-item h3>a')
-
-    for (let i = 0; i < list.length; i++) {
-
-        const url = $(list[i]).attr('href')
-        urlAll.push(url)
-
-    }
-    return urlAll
-
-})()
-    .then(r => {
-        console.log(r)
-        page()
+    const Schema = mongoose.Schema
+    const articleSchema = new Schema({
+        type: String,
+        index: Number,
+        title: {
+            type: String,
+            index: true,
+            unique: true
+        },
+        author: String,
+        time: String,
+        content: Array
     })
-    .catch(e => {
-        console.log('失败啦')
+    const modelSchema = new Schema({
+        index: {
+            type: Number,
+            index: true,
+            unique: true
+        },
+        content: Array
     })
 
+    const articlesModel = mongoose.model('articles', articleSchema)
+    const Model = mongoose.model('Model', modelSchema)
 
-async function page() {
-    const contentArrAll = []
-
-    for (let i = 0; i < urlAll.length; i++) {
-        const request = await axios.get(urlAll[i])
-
+    !(async function getURL() {
+        const request = await axios.get(`https://www.douban.com/group/explore?start=${n}`)
         const document = request.data
 
         const $ = cheerio.load(document)
 
-        const contentArr = []
+        const list = $('.channel-item h3>a')
 
-        let childern = $('.topic-richtext').children()
-
-        for (let i = 0; i < childern.length; i++) {
-            if (childern[i].name === 'p') {
-                let text = $(childern[i]).text()
-                contentArr.push(text)
-            }
-
-            if ($(childern[i]).children().length) {
-                let img = $(childern[i]).find('img').attr('src')
-                contentArr.push(img)
-            } else {
-
-            }
+        for (let i = 0; i < list.length; i++) {
+            const url = $(list[i]).attr('href')
+            urlAll.push(url)
         }
-        contentArrAll.push(contentArr)
-    }
-    console.log(contentArrAll)
+        return urlAll
 
+    })()
+        .then(r => {
+            getArticles()
+        })
+        .catch(e => {
+            console.log('失败啦')
+        })
+
+
+    async function getArticles() {
+        const contentArrAll = []
+
+        for (let i = 0; i < urlAll.length; i++) {
+
+            const request = await axios.get(urlAll[i])
+
+            const document = request.data
+
+            const $ = cheerio.load(document)
+
+            const contentArr = []
+            const title = $('#content h1').text()
+            const author = $('.from a').text()
+            const time = $('.color-green').text()
+            const childern = $('.topic-richtext').children()
+            for (let i = 0; i < childern.length; i++) {
+                if (childern[i].name === 'p') {
+                    let text = $(childern[i]).text()
+                    contentArr.push(text)
+                }
+
+                if ($(childern[i]).children().length) {
+                    let img = $(childern[i]).find('img').attr('src')
+                    contentArr.push(img)
+                }
+            }
+            contentArrAll.push(contentArr)
+
+            articlesModel.create({
+                type: 'selected',
+                index: i + n,
+                title: title,
+                time: time,
+                author: author,
+                content: contentArrAll[i]
+            }, function (err) {
+                if (err) console.log('保存错误')
+            })
+        }
+
+        Model.create({ index: 0, content: contentArrAll }, function (err) {
+            if (err) console.log('保存错误')
+        })
+
+        console.log('保存完毕')
+
+    }
 }
+
+crawler(0)
+
+
+
